aacc347f03ac72c96e67e02dafd78ebb81a52de1
Jadi <jadilmiller@gmail.com>
2023-12-19 17:20:45 -0500

    Fix win/lose triggers

--- level/level.gd
+++ level/level.gd
@@ -1,9 +1,8 @@
 extends Node2D
-@onready var win 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	WinOrLose.win = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
