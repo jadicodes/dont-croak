1a51a3529a07877f74421aeeb117203b766e9e1a
Jadi <jadilmiller@gmail.com>
2023-12-19 14:51:01 -0500

    Move Frog vertically

--- frog/frog.gd
+++ frog/frog.gd
@@ -2,15 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var collided := false
 
 
-#func _physics_process(delta):
-#
-#	collided = move_and_collide()
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-#	move_and_collide()
+func _ready():
+	velocity = Vector2(0, -SPEED)
+
+
+func _physics_process(delta):
+	var collided := move_and_slide()
+	if collided:
+		velocity.y *= -1
